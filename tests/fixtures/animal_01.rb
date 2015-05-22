a63f0f782c4a4c52e6a6f7226a0715f6bafce23c
Tony Machin <crypto@anthonymachin.com>
2015-05-22 21:34:32 +0100

    animal_01.rb commenting removed

--- tests/fixtures/animal_01.rb
+++ tests/fixtures/animal_01.rb
@@ -8,28 +8,28 @@ module AnimalTaxonomy
       tax.instantiate(':animal<:life')
       tax.instantiate(':life<:dog')
       tax.instantiate(':mammal<:animal')
-#      tax.instantiate('[[:carp,:herring]<:fish,:insect]<:animal')
+      #tax.instantiate('[[:carp,:herring]<:fish,:insect]<:animal')
       tax.instantiate('[:carp,:herring]<:fish')
       tax.instantiate('[:fish,:insect]<:animal')
       tax.instantiate(':carpette<:carp<:food')
     else
-      puts "**tag_spec: add_tag(:mouse,:animal)"
+#      puts "**tag_spec: add_tag(:mouse,:animal)"
       tax.add_tag(:mouse,:animal)
-      puts "**tag_spec: add_tag([:cat, :dog], :mammal)"
+#      puts "**tag_spec: add_tag([:cat, :dog], :mammal)"
       tax.add_tags([:cat, :dog], :mammal)
-      puts "**tag_spec: add_tag(:animal, :life)"
+#      puts "**tag_spec: add_tag(:animal, :life)"
       tax.add_tag(:animal, :life)
-      puts "**tag_spec: add_tag(:life, :dog)"
+#      puts "**tag_spec: add_tag(:life, :dog)"
       tax.add_tag(:life, :dog)
-      puts "**tag_spec: add_tag(:mammal, :animal)"
+#      puts "**tag_spec: add_tag(:mammal, :animal)"
       tax.add_tag(:mammal, :animal)
-      puts "**tag_spec: add_tag([:fish, :insect], :animal)"
+#      puts "**tag_spec: add_tag([:fish, :insect], :animal)"
       tax.add_tags([:fish, :insect], :animal)
-      puts "**tag_spec: add_tag([:carp, :herring], :fish)"
+#      puts "**tag_spec: add_tag([:carp, :herring], :fish)"
       tax.add_tags([:carp, :herring], :fish)
-      puts "**tag_spec: add_tag(:carp, :food)"
+#      puts "**tag_spec: add_tag(:carp, :food)"
       tax.add_tag(:carp, :food)
-      puts "tag_spec: add_tag(:carpette, :carp)"
+#      puts "tag_spec: add_tag(:carpette, :carp)"
       tax.add_tag(:carpette, :carp)
     end
     tax.delete_tag(:mammal)

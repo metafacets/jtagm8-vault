94d67bd715e62f7ef5d0620df60a7e475802dccb
Tony Machin <crypto@anthonymachin.com>
2015-06-29 18:53:26 +0100

    server port updated

--- src/app/tagm8_server.rb
+++ src/app/tagm8_server.rb
@@ -2,7 +2,7 @@ require_relative 'facade'
 require 'drb'
 
 Tagm8Db.open('tagm8-app')
-DRb.start_service('druby://127.0.0.1:61665',Facade.instance)
+DRb.start_service('druby://127.0.0.1:61664',Facade.instance)
 puts 'Listening for connection'
 DRb.thread.join
 
